97c21712d6f4728ae6373cd3e9112947a22ae5de
kofta999 <mostafaxxx555@gmail.com>
2025-04-09 13:43:11 +0200

    feat: add get average rating method to business entity

--- src/domain/entities/business.ts
+++ src/domain/entities/business.ts
@@ -1,15 +1,21 @@
+import { Review } from "./review";
+
 export class Business {
   businessId?: number;
   name: string;
   description: string;
 
-  constructor({ name, description }: { name: string; description: string }) {
+  constructor({ name, description }: Pick<Business, "name" | "description">) {
     this.name = name;
     this.description = description;
   }
 
-  calculateAverageRating(): number {
-    return 3;
+  calculateAverageRating(reviews: Review[]): number {
+    if (!reviews || reviews.length === 0) return 0;
+
+    const sum = reviews.reduce((prev, curr) => prev + curr.rating, 0);
+
+    return sum / reviews.length;
   }
 
   generateSlug(): string {

706b6779ad060ed3d4ff4cd8be01878ec7cdcb85
kofta999 <mostafaxxx555@gmail.com>
2025-04-10 17:31:26 +0200

    chore: add logs to business service

--- src/business/business.service.ts
+++ src/business/business.service.ts
@@ -1,11 +1,13 @@
 import type { RegisterBusinessDTO } from "@/common/dtos/create-business.dto";
 import type { GetBusinessReviewsDTO } from "@/common/dtos/get-business-reviews.dto";
 import type { GetBusinessDTO } from "@/common/dtos/get-business.dto";
+import { Logger } from "@/common/util/logger";
 import type { BusinessRepository } from "@/data-access/business.repository";
 import { Business } from "@/domain/entities/business";
 
 export class BusinessService {
   private repository: BusinessRepository;
+  private logger = Logger.getLogger();
 
   constructor(repository: BusinessRepository) {
     this.repository = repository;
@@ -16,6 +18,8 @@ export class BusinessService {
 
     const res = await this.repository.create(business);
 
+    this.logger.info(`Business ${business.name} was registered with ID ${res}`);
+
     return res;
   }
 
@@ -24,6 +28,8 @@ export class BusinessService {
       this.repository.getById(businessId),
       this.repository.getRatings(businessId),
     ]);
+
+    this.logger.info(`Retrieved details for business with ID ${businessId}`);
 
     return {
       businessId,
@@ -35,6 +41,8 @@ export class BusinessService {
 
   async getBusinessReviews(businessId: number): Promise<GetBusinessReviewsDTO> {
     const reviews = await this.repository.getReviews(businessId);
+
+    this.logger.info(`Retrieved reviews for business with ID ${businessId}`);
 
     return reviews;
   }

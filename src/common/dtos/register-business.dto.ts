4aa1b3578dafa67496442a703b38721f08039018
kofta999 <mostafaxxx555@gmail.com>
2025-04-13 13:19:30 +0200

    chore: rename register business dto

--- src/common/dtos/register-business.dto.ts
+++ src/common/dtos/register-business.dto.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const RegisterBusinessSchema = z.object({
   name: z.string(),
   description: z.string(),
+  email: z.string(),
+  password: z.string(),
 });
 
 export type RegisterBusinessDTO = z.infer<typeof RegisterBusinessSchema>;

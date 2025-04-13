e5727a435be90fe9577fda3d9b4944b05a81d229
kofta999 <mostafaxxx555@gmail.com>
2025-04-13 12:08:56 +0200

    feat: add user table

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -1,7 +1,19 @@
+CREATE TYPE UserRole AS ENUM ('BUSINESS', 'REVIEWER', 'ADMIN');
+
+CREATE TABLE "user" (
+    "user_id" SERIAL PRIMARY KEY,
+    "email" VARCHAR UNIQUE NOT NULL,
+    "password" VARCHAR NOT NULL,
+    "role" UserRole NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE "business" (
     "business_id" SERIAL PRIMARY KEY,
+    "user_id" INT NOT NULL,
     "name" VARCHAR NOT NULL,
-    "description" VARCHAR NOT NULL
+    "description" VARCHAR NOT NULL,
+    FOREIGN KEY ("user_id") REFERENCES "user" ("user_id")
 );
 
 CREATE TABLE "review" (
@@ -13,10 +25,4 @@ CREATE TABLE "review" (
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY ("business_id") REFERENCES "business" ("business_id"),
     CHECK ("rating" BETWEEN 1 AND 5)
-);
-
-CREATE TABLE "admin" (
-    "admin_id" SERIAL PRIMARY KEY,
-    "email" VARCHAR NOT NULL,
-    "password" VARCHAR NOT NULL
 );

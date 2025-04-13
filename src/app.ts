eb100a4a86333f3b222e315a1fb3cefe96485a7a
kofta999 <mostafaxxx555@gmail.com>
2025-04-13 13:20:59 +0200

    feat: update register route to register business user

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import { BusinessRepository } from "@/data-access/business.repository";
 import { BusinessController } from "@/presentation/controllers/business.controller";
 import * as businessRoutes from "@/presentation/routes/business.routes";
 import { Pool } from "pg";
+import { BunPasswordService } from "./business/bun-password.service";
 import { BusinessService } from "./business/business.service";
 import { ReviewService } from "./business/review.service";
+import { UserService } from "./business/user.service";
 import { errorHandler } from "./common/middleware/error-handler";
 import { loggerMiddleware } from "./common/middleware/pino-logger";
 import type { AppOpenAPI } from "./common/types";
 import { ReviewRepository } from "./data-access/review.repository";
+import { UserRepository } from "./data-access/user.repository";
 
 function injectDeps(app: AppOpenAPI) {
   const pool = new Pool({
@@ -23,18 +26,22 @@ function injectDeps(app: AppOpenAPI) {
   // Repositories
   const businessRepository = new BusinessRepository(pool);
   const reviewRepository = new ReviewRepository(pool);
+  const userRepository = new UserRepository(pool);
 
   // Services
+  const passwordService = new BunPasswordService();
   const businessService = new BusinessService(
     businessRepository,
     reviewRepository,
   );
   const reviewService = new ReviewService(reviewRepository);
+  const userService = new UserService(userRepository, passwordService);
 
   // Controllers
   const businessController = new BusinessController(
     businessService,
     reviewService,
+    userService,
   );
 
   // Routers
